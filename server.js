781308a97cc6508bc32335af068745950e13d070
DannyPoizot <poizot.dan@gmail.com>
2022-06-15 13:08:40 +0200

    moving folders

--- server.js
+++ server.js
@@ -49,3 +49,7 @@ function initial() {
         name: "admin"
     });
 }
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+// set port, listen for requests
