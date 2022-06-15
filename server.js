426eda07f9f887eccedd6aa4574b5a6d61388585
DannyPoizot <poizot.dan@gmail.com>
2022-06-15 09:33:59 +0200

    add auth

--- server.js
+++ server.js
@@ -23,3 +23,29 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+const db = require("./app/models");
+const Role = db.role;
+db.sequelize.sync({
+    force: true
+}).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
