bbc9edf390b6f06b56c7337f43e437d2ba9e01c3
DannyPoizot <poizot.dan@gmail.com>
2022-06-22 18:21:03 +0200

    updating img logo

--- frontend/frontend/components/Header.js
+++ frontend/frontend/components/Header.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Image from 'next/image';
+import LogoGlt from '../public/LogoGlt.png'
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                <h1>Header</h1>
-            </div>
+            <header>
+                <div>
+                    <Image 
+                        src={LogoGlt}
+                        alt= "Logo GreenLandTech"
+                    />
+                </div>
+            </header>
         )
     }
 }
