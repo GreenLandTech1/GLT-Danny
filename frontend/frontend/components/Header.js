ab4a73c57999f597ac2bc4655f0483b5bb0d44e5
DannyPoizot <poizot.dan@gmail.com>
2022-06-22 20:55:28 +0200

    header updating + nav

--- frontend/frontend/components/Header.js
+++ frontend/frontend/components/Header.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import Image from 'next/image';
-import LogoGlt from '../public/LogoGlt.png'
+import LogoGlt from '../public/LogoGlt.png';
+import login from '../public/login.jpg'
 
 export default class Header extends Component {
     render() {
         return (
             <header>
                 <div>
-                    <Image 
-                        src={LogoGlt}
-                        alt= "Logo GreenLandTech"
-                    />
+                    <div>
+                        <Image 
+                            src={LogoGlt}
+                            alt= "Logo GreenLandTech"
+                        />
+                    </div>
+
+                    <nav>
+                        <ul>
+                            <li><a href='#'>Home</a></li>
+                            <li><a href='#'>About</a></li>
+                            <li><a href='#'>Contact</a></li>
+                            <li><a href='#'>
+                                <Image
+                                    src={login}
+                                />
+                            </a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div>
+                    
                 </div>
             </header>
         )
