6e4464420fa6fee0eeeaa521c74159e262d23272
DannyPoizot <poizot.dan@gmail.com>
2022-06-29 16:08:22 +0200

    starting header

---
+++ frontend/frontend/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <header>
+                    <div>
+                        <img src="/home/danny/Desktop/GLT-Danny/frontend/frontend/public/images/LogoGlt.png" alt="Logo GreenLandTech" />
+                    </div>
+            </header>
+        </div>
+    );
+}
+
+export default Header;  
