9ad8f26e3906dd6b3141032eb63c1e9f0d695860
DannyPoizot <poizot.dan@gmail.com>
2022-06-30 18:48:31 +0200

    update header

--- frontend/frontend/src/components/Header.jsx
+++ frontend/frontend/src/components/Header.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import LogoGlt from './../images/LogoGlt.png'
+import login from './../images/login.png'
 
 
 function Header() {
     return (
         <div>
             <header>
+                <ul>
+                    <div>
+                        <img src="" alt="Logo" />
+                    </div>
+                    <div>
+                        <img src={login} alt='Connexion' />
+                    </div>
+                </ul>
                     <div>
                         <img src={LogoGlt} alt="Logo GreenLandTech" />
                     </div>
-
-                    
             </header>
         </div>
     );
