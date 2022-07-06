d6e0f7aa9ce6bdda685a639e7b0a9c011b5c3925
DannyPoizot <poizot.dan@gmail.com>
2022-07-06 22:54:27 +0200

    who are are section

--- frontend/frontend/src/components/Whoarewe.jsx
+++ frontend/frontend/src/components/Whoarewe.jsx
@@ -6,12 +6,16 @@ function Whoarewe() {
         <section>
             <aside>
                 <article>
-
+                    <img></img>
+                    <img></img>
+                    <img></img>
+                    <img></img>
+                    <img></img>
                 </article>
             </aside>
 
-            <aside className='prose'>
-                <h3>Lorem Ipsum</h3>
+            <aside className='w-1 flex flex-col items-start'>
+                <h3 className='text-xl'>Lorem Ipsum</h3>
                 <p>Quis ullamco sit aliquip do velit veniam tempor laboris nulla Lorem sit sunt laborum duis. Proident qui aliqua aute exercitation ullamco nostrud cupidatat excepteur consectetur. Tempor Lorem anim laborum id esse est sunt ad dolor. In cillum laboris ipsum in in sint adipisicing quis incididunt nisi reprehenderit eiusmod consectetur magna. Dolore elit amet sunt veniam nostrud deserunt culpa id.</p>
             </aside>
         </section>
