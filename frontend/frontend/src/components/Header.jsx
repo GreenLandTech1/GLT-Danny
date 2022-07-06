fb8dfdf7d1a438fdf0d2ced991eff52c75de4ab9
DannyPoizot <poizot.dan@gmail.com>
2022-07-06 17:13:57 +0200

    header css debut

--- frontend/frontend/src/components/Header.jsx
+++ frontend/frontend/src/components/Header.jsx
@@ -2,25 +2,42 @@ import React from 'react';
 import LogoGlt from './../images/LogoGlt.png'
 import login from './../images/login.png'
 import logoNav from './../images/logoNav.jpeg';
-import '.././header.css'
+import '.././index.css';
+import '.././header.css';
 
 
 function Header() {
     return (
         <header>
-            <nav>
-                <ul>
-                    <div>
-                        <img  className='logo2' src={logoNav} alt="Logo" />
+            <nav classNameName="bg-white shadow">
+                <div className="container mx-auto px-6 py-3 ">
+                    <div className="md:flex md:items-center md:justify-between">
+                        <div className="flex justify-between items-center">
+                            <div className="text-xl font-semibold text-gray-700">
+                                <a href="#" className="text-gray-800 text-xl font-bold hover:text-gray-700 md:text-2xl"><img src={logoNav}/></a>
+                            </div>
+
+                            <div className="flex md:hidden">
+                                <button type="button" className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
+                                    <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+                                        <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className="hidden -mx-4 md:flex md:items-center">
+                            <a href="#" className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"><img className='login' src={login} /></a>
+                        </div>
                     </div>
-                    <div className='login'>
-                        <img className='log-img' src={login} alt='Connexion' />
-                    </div>
-                </ul>
-                    <div className='logo1' >
-                        <img src={LogoGlt} alt="Logo GreenLandTech" />
-                    </div>
+                </div>
             </nav>
+
+            <div className="w-full bg-cover bg-center" style={{height:32 }}>
+                <div className="flex items-center justify-center h-full w-full">
+                    <img src={LogoGlt} />
+                </div>
+            </div>
         </header>
     );
 }
--- frontend/frontend/src/components/Whoarewe.jsx
+++ frontend/frontend/src/components/Whoarewe.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Whoarewe(props) {
     return (
-        <div>
+        <section>
             <p className='text-center'>Who are we ?</p>
-        </div>
+        </section>
     );
 }
 
